0b9e2d8bb3a8573d7ab33fbbe58b545a0846744b
Matt Davis <jiffyclub@gmail.com>
2015-03-07 21:53:13 -0800

    include version number in tservice package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='tservice',
-    version='1.0dev',
+    version='1.0.0dev',
     description=(
         'Quick static file server'),
     long_description=long_description,
